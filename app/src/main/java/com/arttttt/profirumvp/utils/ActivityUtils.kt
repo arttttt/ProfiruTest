5faeb0ebfdeeb2d3c56bdc8e71cb808dd9387544
arttttt <artem-bambalov@yandex.ru>
2018-12-13 19:11:10 +0300

    code styling
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- app/src/main/java/com/arttttt/profirumvp/utils/ActivityUtils.kt
+++ app/src/main/java/com/arttttt/profirumvp/utils/ActivityUtils.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 object ActivityUtils {
-    inline fun <reified T: AppCompatActivity>startActivity(context: Context, activityOptions: Bundle?, intentExtra: Bundle?,
+    inline fun <reified T: AppCompatActivity>startActivity(context: Context,
+                                                           activityOptions: Bundle?,
+                                                           intentExtra: Bundle?,
                                                            flags: Int) {
         val intent = Intent(context, T::class.java).apply {
-            this.flags = flags
+            this.flags = this.flags or flags
             if (intentExtra != null)
                 this.putExtras(intentExtra)
         }
