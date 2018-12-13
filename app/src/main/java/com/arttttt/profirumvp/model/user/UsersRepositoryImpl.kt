50a8bee42797e54af01638a0edd342301805769b
arttttt <artem-bambalov@yandex.ru>
2018-12-13 19:10:54 +0300

    use array list instead of set in UsersRepositoryImpl
    Signed-off-by: arttttt <artem-bambalov@yandex.ru>

--- app/src/main/java/com/arttttt/profirumvp/model/user/UsersRepositoryImpl.kt
+++ app/src/main/java/com/arttttt/profirumvp/model/user/UsersRepositoryImpl.kt
@@ -4,11 +4,11 @@ import com.arttttt.profirumvp.model.user.base.UsersDataSource
 import com.arttttt.profirumvp.model.user.base.UsersRepository
 
 class UsersRepositoryImpl(private val dataSource: UsersDataSource): UsersRepository {
-    private val users = linkedSetOf<User>()
+    private val users = mutableListOf<User>()
 
     override fun getUsers(onCompletion: (List<User>) -> Unit, onError: (String) -> Unit) {
         if (users.isNotEmpty()) {
-            onCompletion(users.toList())
+            onCompletion(users)
             return
         }
 
